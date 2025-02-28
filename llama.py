a2aef0a1aad4557962cfc45fdbaa12551e2c5f89
FoppeFN <nrfoppe@gmail.com>
2025-02-28 00:20:01 -0600

    update all files

--- llama.py
+++ llama.py
@@ -1,18 +1,23 @@
-import ollama
+import ollama #imports ollama library
 
-client = ollama.Client()
-model = "llama3.2:1b"
+client = ollama.Client() #sets client to instance of ollama client
+model = "llama3.2:1b" #sets the model to llama3.2:1b
 
 
-with open("input.txt", "r") as file:
+with open("input.txt", "r") as file: #open input.txt and reads it line by line, with each line being an index in a list.
     prompt = file.readlines()
-
     
-with open("outputllama.txt", "w") as output_file:
-    for language_output in prompt:
-        response_prompt = ("Classify the following headline as Negative, Neutral, or Positive, provide only a one word response, this is the headline:" + language_output)
-        response = client.generate(model=model, prompt=response_prompt)
-        output_file.write(response.response + "\n")
+with open("outputllamabasic.txt", "w") as output_file, open ("junkllamabasic.txt", "w" ) as junk_file: #opens two files and sets them to write, one being the junk file, the other being the file with the output we care about.
+    for language_output in prompt: #sets a for loop reading in the lines from input.txt
+        response_prompt = ("Classify the following headline as Negative, Neutral, or Positive, provide only a one word response. The following sentence is the headline:" + language_output) #sets response_prompt to a question + the line we are feeding in, so in the first instance of the loop, the a line gets read in, then b line.
+        response = client.generate(model=model, prompt=response_prompt) # this puts the response in which the client generates to a variable named response, we pass in the model and the prompt.
+        # print(response.response) // for debugging purposes
+        if response.response.lower() in ["positive", "negative", "neutral"]: #this simply makes the response.response to lower cause and then checks if it is one of our words we are looking for and if it is we print to our output_file, this code works perfectly since our model only prints in one word, our phi is a different case.
+            output_file.write(response.response + "\n")
+        else: #writes to junk file if any other word except "positive" "neutral" or "negative" is spotted (not really needed since model only prints one word).
+            junk_file.write(response.response + "\n")
+        
+    
 
         
 
--- phi.py
+++ phi.py
@@ -1,19 +1,35 @@
-import ollama
+import ollama #import ollama library
+
+client = ollama.Client() #sets client up
+model = "phi3:latest" #sets model to phi3
 
 
-
-client = ollama.Client()
-with open("input.txt", "r") as file:
-    content = file.read()
+with open("input.txt", "r") as file: #opens up input.txt
+    prompt = file.readlines() #reads prompt into a list, where each line is an index in the list.
 
     
-model = "phi3:latest"
-prompt = f"Give me a one word response for whether each headline A, B, and C are positive negative or neutral, no explanation or context needed. {content}"
+with open("outputphi.txt", "w", encoding = "utf=8") as output_file: #opens the output.phi text file
+    for language_output in prompt: #starts a for loop that goes through each prompt, starting with a and ending with c.
+        response_prompt = f"is this statement: {language_output} positive, negative, or neutral? answer in one word." # sets what we are prompting the llm
+        response = client.generate(model=model, prompt=response_prompt)
+        #print(response.response) // code for debugging purposses
+        if response.response.lower() in ["positive", "negative", "neutral"]: #similiar to our llama code, if the first word that is printed out by our llm, it will be saved to our parsed data file and a new line will be printed, if not then the else will be activated.
+            output_file.write(response.response + "\n")
+        else:
+            split_response = (response.response.lower()).split() #sets a new variable called split response, that first converts it to a string from a list, then splits the 
+            print(split_response)
+            for parsed_data in split_response: #creates a for loop to iterate through our string of split responses, which could be a very long paragraph of data.
+                    if parsed_data in ["positive","negative","neutral"]: #if that word is found, the if block is activated
+                        output_file.write(parsed_data + "\n") #writes the data to our parsed data file
+                        split_response.remove(parsed_data) #removes that word from our split_response, as we have already found it and do not want to find it again.
+                        break #breaks the for loop and moves to print out the rest of the garbage text, as we have found connotation word.
+            output_rebuild = "" # ensures their is a space in between each word as we rebuild our garbage text output.
+            for rebuild in split_response:
+                print(rebuild)
+                output_rebuild = output_rebuild + " " + rebuild #this line rebuilds the garbage text data that the LLM is outputting
+            output_rebuild += "\n" # in case the LLM writes two long worded responses, this will create a new line in the junk file between the two
+            print(output_rebuild)
 
-response = client.generate(model=model, prompt=prompt)
-
-
-output_file = "outputphi.txt"
-
-with open(output_file, "w") as file:
-    file.write(response.response)
+            junk_file = open("junkphi.txt", "w", encoding = "utf-8")
+            junk_file.write(output_rebuild)
+            junk_file.close()
